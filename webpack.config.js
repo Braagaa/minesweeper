2166df1c9fad5642c92afac5633c7cc0d8bf6b83
Michael Braga <michael_braga@outlook.com>
2020-04-24 21:33:00 -0400

    Inital commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const {resolve} = require('path');
+
+module.exports = {
+	devtool: "source-map",
+	entry: {
+		app: ["./src/index.tsx"],
+		vendor: ["react", "react-dom"]
+	},
+	output: {
+		path: resolve(__dirname, 'public'),
+		filename: 'js/[name].bundle.js'
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", '.js']
+	},
+	devServer: {
+		contentBase: resolve(__dirname, 'public'),
+		hot: true,
+		port: 3000,
+		open: true,
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+			{
+				test: /.(js|ts|tsx)$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					cache: true
+				}
+			},
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [{loader: "ts-loader"}]
+			}
+		]
+	}
+};
